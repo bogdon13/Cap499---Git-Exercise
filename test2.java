7fddea0cd56ab7d842930ffab581a275213f04c3
Bantella <daven89a@gmail.com>
2021-09-16 21:54:37 -0700

    adding second file

---
+++ test2.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		String[] array = {"Adam", "Darren", "Cole", "Georgia", "Bailey" };
+		
+		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
+		}
+	}
+
+
